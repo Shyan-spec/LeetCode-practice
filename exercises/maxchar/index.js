9a2f9b798a1d822a75bb2ddb48fd22a26efb43f3
Shyan W. <shyanw321@gmail.com>
2024-02-19 23:42:26 -0500

    FInished MaxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,34 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+   let numSet = new Set();
+   let maxKey;
+   let max = 0;
 
+ let arr = str.split('')
+
+ for(let i = 0; i < arr.length ; i++){
+    if(arr[i] in numSet) {
+        numSet[arr[i]] += 1
+       max = Math.max(max, numSet[arr[i]])
+    }
+    else{
+        numSet[arr[i]] = 1
+       max = Math.max(max, numSet[arr[i]])
+    }
+    
+        if (numSet[arr[i]] == max){
+            maxKey = arr[i]
+        }
+ }
+
+return maxKey
+
+    
+
+}
+
+maxChar("abcccccccd") === "c"
+maxChar("apple 1231111") === "1"
 module.exports = maxChar;
