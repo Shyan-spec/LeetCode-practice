3b978e892b37aa9497d33dfd348a9d60deb899da
Shyan W. <shyanw321@gmail.com>
2024-02-28 16:23:41 -0500

    Finished anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,31 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+
+   
+    let arr1 = stringA.replace(/[^a-zA-z]/g, '').split("")
+    let arr2 = stringB.replace(/[^a-zA-z]/g, '').split("")
+    
+    let sorted1 = arr1.sort().join('').toLowerCase()
+    let sorted2 = arr2.sort().join('').toLowerCase()
+    
+    console.log(sorted1)
+    console.log(sorted2)
+
+    if(sorted1 === sorted2){
+        console.log(true)
+        return true
+    }
+    else {
+        console.log(false)
+        return false
+    }
+}
+
+anagrams('rail safety', 'fairy tales')
+anagrams('RAIL! SAFETY!', 'fairy tales')
+anagrams('Hi there', 'Bye there')
+anagrams('Hi! Whoa!', 'Whoa! Hi!')
 
 module.exports = anagrams;
